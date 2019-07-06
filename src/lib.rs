e05f83d52ca3f0d4b8f415600a8c8fc401c045c0
chux0519 <chuxdesign@hotmail.com>
2019-07-06 11:01:11 +0800

    setup error pattern

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 //! kvs implement an in memory k-v store
 
 use std::collections::HashMap;
+use std::path::Path;
+use failure::Error;
 
 /// The struct KvStore stores k-v string pairs
 /// implemented with std::collections::HashMap, totally in memory
@@ -20,28 +22,35 @@ pub struct KvStore {
     store: HashMap<String, String>,
 }
 
+/// Short for Result<T, Box<std::error::Error>>
+pub type Result<T> = std::result::Result<T, Error>;
+
 impl KvStore {
     /// constructor
-    pub fn new() -> Self {
-        KvStore {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let store = KvStore {
             store: HashMap::new(),
-        }
+        };
+        Ok(store)
     }
 
     /// Set a k-v pair
     /// Behavior is exactly the same as hashmap.insert
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.store.insert(key, value);
+        Ok(())
     }
 
     /// Get value of key
     /// Returns None if key is not exists
-    pub fn get(&mut self, key: String) -> Option<String> {
-        self.store.get(&key).cloned()
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        let res = self.store.get(&key).cloned();
+        Ok(res)
     }
 
     /// Remove a key
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.store.remove(&key);
+        Ok(())
     }
 }
